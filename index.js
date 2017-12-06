0547f4babe866c59034880326609f51575c43128
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2017-12-06 18:07:12 -0200

    Fixed errors when running the production build. Needs more testing though

--- index.js
+++ index.js
@@ -17,7 +17,17 @@ module.exports = (options = {}) => (config, env) => {
 	/** @type {*} */
 	const loader = getLoader(config.module.rules, rule => rule.test && rule.test.toString().indexOf('.css') !== -1);
 
-	const cssLoader = loader && loader.use.filter(l => l.loader && l.loader.indexOf(`${path.sep}css-loader${path.sep}`) !== -1)[0];
+	if(!loader) throw new Error('css loader not found');
+
+	if(!(loader.use || loader.loader)) {
+		throw new Error('Could find the css-loader');
+	}
+
+	const cssLoader = (loader.use || loader.loader).filter(l => l.loader && l.loader.indexOf(`${path.sep}css-loader${path.sep}`) !== -1)[0];
+
+	if(!cssLoader) {
+		throw new Error('Could find the css-loader');
+	}
 
 	cssLoader.options = {
 		...cssLoader.options,
